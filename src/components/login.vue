bd5b5c515260bd0afd653760ed8c47d9a5ea7328
fanjixiang <2669774576@qq.com>
2021-05-10 08:51:05 +0800

    项目创建完成

---
+++ src/components/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login_box">
+
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="less" scoped>
+.login_box{
+  width: 200px;
+  height: 200px;
+  background-color: #42b983;
+}
+</style>
