4bbdd4d0510d76b9278fc2010b65e4aec3ba4454
fanjixiang <2669774576@qq.com>
2021-05-19 15:11:35 +0800

    添加密码框输入隐藏

--- src/components/login.vue
+++ src/components/login.vue
@@ -10,7 +10,7 @@
             <el-input placeholder="请输入账号" v-model="loginForm.username"></el-input>
           </el-form-item>
           <el-form-item prop="password">
-            <el-input placeholder="请输入密码" v-model="loginForm.password"></el-input>
+            <el-input placeholder="请输入密码" v-model="loginForm.password" show-password></el-input>
           </el-form-item>
           <el-form-item class="btns">
             <el-button type="primary" @click="login">登录</el-button>
